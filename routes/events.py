77b0fde202b2136fb80fac18f780b16bde561952
Ufuk Yıldız <84462888+yldzufukk@users.noreply.github.com>
2023-05-12 19:20:51 +0300

    Add files via upload

---
+++ routes/events.py
@@ -0,0 +1,81 @@
+
+
+from fastapi import APIRouter, Depends, Body, HTTPException, status, Request
+from database.connection import get_session
+from models.events import Event, EventUpdate
+from typing import List
+from sqlmodel import select
+
+event_router = APIRouter(
+    tags=["Events"]
+)
+
+events = []
+
+@event_router.get("/", response_model=List[Event])
+async def retrieve_all_event(session= Depends(get_session)) -> List[Event]:
+    statement = select(Event)
+    events = session.exec(statement).all()
+    return events
+
+@event_router.get("/{id}", response_model=Event)
+async def retrieve_event(id: int, session= Depends(get_session)) -> Event:
+    event = session.get(Event, id)
+    if event:
+        return event
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Event with supplied ID does not exist"
+    )
+
+
+@event_router.post("/new")
+async def create_event(new_event: Event, session= Depends(get_session)) -> dict:
+    session.add(new_event)
+    session.commit()
+    session.refresh(new_event)
+    
+    return {
+        "message" : "Event created successfully"
+    }
+
+@event_router.delete("/{id}")
+async def delete_event(id: int, session= Depends(get_session)) -> dict:
+    event = session.get(Event,id)
+    if event:
+        session.delete(event)
+        session.commit()
+        
+        return {"message" : "Event deleted succesfully" }
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Event with supplied ID does not exist"
+    )
+
+
+@event_router.delete("/")
+async def delete_all_events() -> dict:
+    events.clear()
+    return {
+        "message": "Events deleted successfully"
+    }
+
+@event_router.put("/edit/{id}", response_model= Event)
+async def update_event(id: int, new_data: EventUpdate, session= Depends(get_session)) -> Event:
+    event = session.get(Event, id)
+
+    if event:
+        event_data = new_data.dict(exclude_unset= True)
+        for key,value in event_data.items():
+            setattr(event, key, value)
+
+        session.add(event)
+        session.commit()
+        session.refresh(event)
+        return event
+    
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Event with supplied ID does not exist"
+    )
